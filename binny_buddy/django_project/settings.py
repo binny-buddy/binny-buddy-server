5314d1370a64a84f0ba98dce9edbacc982d0901e
tonynamy <me@tonynamy.dev>
2025-04-13 16:56:03 +0900

    django setup

--- binny_buddy/django_project/settings.py
+++ binny_buddy/django_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-z0z!r5@!-#i1q5_k!_983nk2y)ilf3=mmao13fq^j-dukc%!yy
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -102,13 +102,13 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ko-kr'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Seoul'
 
 USE_I18N = True
 
-USE_TZ = True
+USE_TZ = False
 
 
 # Static files (CSS, JavaScript, Images)
