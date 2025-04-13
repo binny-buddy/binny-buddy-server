a0c9797a59aeeaa3cd87c162a288439bf5cac499
tonynamy <me@tonynamy.dev>
2025-04-13 16:55:13 +0900

    django setup

---
+++ binny_buddy/django_project/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for binny_buddy project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'binny_buddy.django_project.settings')
+
+application = get_asgi_application()
