39fe3a4fc334b95381ec4b914d3c79b0c51dd57c
tonynamy <me@tonynamy.dev>
2025-05-16 17:52:47 +0900

    cors

--- binny_buddy/django_project/settings.py
+++ binny_buddy/django_project/settings.py
@@ -45,10 +45,12 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "corsheaders",
     "binny_buddy.apps.core",
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -57,6 +59,8 @@ MIDDLEWARE = [
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
+
+CORS_ORIGIN_ALLOW_ALL = True
 
 ROOT_URLCONF = "binny_buddy.django_project.urls"
 
